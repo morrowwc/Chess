404dbc8e169a0fefe21e767d7d4820ac445756c8
morrowwc <65681991+morrowwc@users.noreply.github.com>
2021-01-29 10:26:23 -0600

    Fixes and Organization
    I moved the Piece intitialization fucntion out of line as well as the Chess::start function.
    
    I fixed all the child Piece intializtions.

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Chess
 I'll be making chess
+
+This is my first project using github, so I am not really sure how this works.
--- chess.h
+++ chess.h
@@ -8,69 +8,76 @@ class Piece{
         int x;
         int y;
     public:
-        Piece(bool w, int _x, int _y){taken = false; white = w ;x = _x; y = _y;}
-        bool move(int x, int y) = 0;
+        Piece(bool w, int _x, int _y);
+        virtual bool move(int x, int y) = 0;
     
 };
 class Pawn : public Piece{
     public:
-        Pawn(bool w, int _x, int _y) : Piece(bool w, int _x, int _y){}
+        Pawn(bool w, int _x, int _y) : Piece(w, _x, _y){}
         bool move(int x, int y);
 };
 class Rook : public Piece{
     public:
-        Rook(bool w, int _x, int _y) : Piece(bool w, int _x, int _y){}
+        Rook(bool w, int _x, int _y) : Piece(w, _x, _y){}
         bool move(int x, int y);
 };
 class Bishop : public Piece{
     public:
-        Bishop(bool w, int _x, int _y) : Piece(bool w, int _x, int _y){}
+        Bishop(bool w, int _x, int _y) : Piece(w, _x, _y){}
         bool move(int x, int y);
 };
 class Knight : public Piece{
     public:
-        Knight(bool w, int _x, int _y) : Piece(bool w, int _x, int _y){}
+        Knight(bool w, int _x, int _y) : Piece(w, _x, _y){}
         bool move(int x, int y);
 };
 class Queen : public Piece{
     public:
-        Queen(bool w, int _x, int _y) : Piece(bool w, int _x, int _y){}
+        Queen(bool w, int _x, int _y) : Piece(w, _x, _y){}
         bool move(int x, int y);
 };
 class King : public Piece{
     public:
-        King(bool w, int _x, int _y) : Piece(bool w, int _x, int _y){}
+        King(bool w, int _x, int _y) : Piece(w, _x, _y){}
         bool move(int x, int y);
 };
 
 class Chess{
-    void start(){
-        Piece* set[2][16]; 
-        for(int i = 0, i < 2, i++){
-            for(int x = 0, x < 8, x++){
-                int y = 1;
-                if (i == 0){
-                    y += 5
-                }
-                set[i][j] = Pawn(i,x,y)
-            }
-        }
-        for (int i = 0; i < 2; i++){
-            int y = 0;
-            if (i == 0){
-                y += 7
-            }
-            set[i][8]  = Rook(i,0,y);
-            set[i][9]  = Knight(i,1,y);
-            set[i][10] = Bishop(i,2,y);
-            set[i][11] = King(i,3,y);
-            set[i][12] = Queen(i,4,y);
-            set[i][13] = Bishop(i,5,y);
-            set[i][14] = Knight(i,6,y);
-            set[i][15] = Rook(i,7,y);
-
-        }
-    };
+    void start();
     bool isOver();
 };
 
+Piece::Piece(bool w, int _x, int _y){
+    taken = false; 
+    white = w ;x = _x; 
+    y = _y;
+}
+
+void Chess::start(){
+    Piece* set[2][16]; 
+    for(int i = 0; i < 2; i++){
+        for(int x = 0; x < 8; x++){
+            int y = 1;
+            if (i == 0){
+                y += 5;
+            }
+            set[i][x] = new Pawn(i,x,y);
+        }
+    }
+    for (int i = 0; i < 2; i++){
+        int y = 0;
+        if (i == 0){
+            y += 7;
+        }
+        set[i][8]  = new Rook(i,0,y);
+        set[i][9]  = new Knight(i,1,y);
+        set[i][10] = new Bishop(i,2,y);
+        set[i][11] = new King(i,3,y);
+        set[i][12] = new Queen(i,4,y);
+        set[i][13] = new Bishop(i,5,y);
+        set[i][14] = new Knight(i,6,y);
+        set[i][15] = new Rook(i,7,y);
+
+    }
+};
