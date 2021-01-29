33ebe36e99e5f1a2767222fb0f32ba8612d9331b
morrowwc <65681991+morrowwc@users.noreply.github.com>
2021-01-29 13:31:05 -0600

    Update chess.h

--- chess.h
+++ chess.h
@@ -1,5 +1,5 @@
 #include<string>
-
+#include<iostream>
 
 class Piece{
     private:
@@ -7,55 +7,66 @@ class Piece{
         bool taken;
         int x;
         int y;
+        char symbol;
     public:
-        Piece(bool w, int _x, int _y);
+        Piece(bool w, int _x, int _y, char s);
         virtual bool move(int x, int y) = 0;
     
 };
 class Pawn : public Piece{
     public:
-        Pawn(bool w, int _x, int _y) : Piece(w, _x, _y){}
+        Pawn(bool w, int _x, int _y) : Piece(w, _x, _y, 'p'){}
         bool move(int x, int y);
 };
 class Rook : public Piece{
     public:
-        Rook(bool w, int _x, int _y) : Piece(w, _x, _y){}
+        Rook(bool w, int _x, int _y) : Piece(w, _x, _y, 'R'){}
         bool move(int x, int y);
 };
 class Bishop : public Piece{
     public:
-        Bishop(bool w, int _x, int _y) : Piece(w, _x, _y){}
+        Bishop(bool w, int _x, int _y) : Piece(w, _x, _y, 'B'){}
         bool move(int x, int y);
 };
 class Knight : public Piece{
     public:
-        Knight(bool w, int _x, int _y) : Piece(w, _x, _y){}
+        Knight(bool w, int _x, int _y) : Piece(w, _x, _y, 'N'){}
         bool move(int x, int y);
 };
 class Queen : public Piece{
     public:
-        Queen(bool w, int _x, int _y) : Piece(w, _x, _y){}
+        Queen(bool w, int _x, int _y) : Piece(w, _x, _y, 'Q'){}
         bool move(int x, int y);
 };
 class King : public Piece{
     public:
-        King(bool w, int _x, int _y) : Piece(w, _x, _y){}
+        King(bool w, int _x, int _y) : Piece(w, _x, _y, 'K'){}
         bool move(int x, int y);
 };
 
 class Chess{
-    void start();
-    bool isOver();
+    private:
+        Piece* set[2][16];
+        char board[8][8];
+    public:
+        void start();
+        void printBoard();
+        bool isOver();
 };
 
-Piece::Piece(bool w, int _x, int _y){
+Piece::Piece(bool w, int _x, int _y, char s){
     taken = false; 
     white = w ;x = _x; 
     y = _y;
+    symbol = s;
 }
-
 void Chess::start(){
-    Piece* set[2][16]; 
+    for (int i = 0; i < 8; i++){
+        for (int ii = 0; ii < 8; ii++){
+            board[i][ii] = ' ';
+        }
+    }
+    
     for(int i = 0; i < 2; i++){
         for(int x = 0; x < 8; x++){
             int y = 1;
@@ -71,13 +82,18 @@ void Chess::start(){
             y += 7;
         }
         set[i][8]  = new Rook(i,0,y);
-        set[i][9]  = new Knight(i,1,y);
-        set[i][10] = new Bishop(i,2,y);
-        set[i][11] = new King(i,3,y);
-        set[i][12] = new Queen(i,4,y);
+        set[i][10]  = new Knight(i,1,y);
+        set[i][12] = new Bishop(i,2,y);
+        set[i][15] = new King(i,3,y);
+        set[i][14] = new Queen(i,4,y);
         set[i][13] = new Bishop(i,5,y);
-        set[i][14] = new Knight(i,6,y);
-        set[i][15] = new Rook(i,7,y);
-
+        set[i][11] = new Knight(i,6,y);
+        set[i][9] = new Rook(i,7,y);
     }
 };
+void printBoard(){
+    std::cout << "-----------------"
+
+}
+bool Chess::isOver(){
+}
